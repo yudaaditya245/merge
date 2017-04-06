6b08fa40a599373eba260050840c062a596bcf9b
Muhammad Rifan <muhammadrifan153@gmail.com>
2017-04-06 15:39:00 +0700

    tambah

---
+++ tambah.php
@@ -0,0 +1,8 @@
+<form id="tambah" method="get" action="index2.php">
+  <label>Nama</label><br>
+  <input type="text" name="nm" /><br>
+  <label>Jenis Kelamin </label><br>
+  <input type="text" name="jk" /><br>
+
+  <input type="submit" name="kirim" value="Kirim!" />
+</form>
